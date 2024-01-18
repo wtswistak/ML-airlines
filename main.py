ed1d1e3ed851c483a14cdd19838e6a0acb2fcea1
wojtek <wswistak@student.agh.edu.pl>
2024-01-18 23:14:30 +0100

    transform data, delete cols

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from tensorflow import keras
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-print("Tensorflow version: ", tf.__version__)
+
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
   try:
@@ -25,12 +25,28 @@ if gpus:
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-train["Arrival Delay in Minutes"].fillna(0, inplace=True)
+
+
+#wypelnienie pustych rekordow
+train["Arrival Delay in Minutes"].fillna(0, inplace=True) 
 test["Arrival Delay in Minutes"].fillna(0, inplace=True)
 
+
+# print(test.nunique())
+
+#usuniecie konkretnych kolumn
+test.drop(labels = ["Unnamed: 0","id", "Gate location" ], axis = 1, inplace=True)
+train.drop(labels = ["Unnamed: 0","id",  "Gate location"], axis = 1,inplace=True)
+
+
+#kodowanie tekstu na liczby unikalne całkowite
 label_encoder_gender = LabelEncoder()
 train['Gender'] = label_encoder_gender.fit_transform(train['Gender'])
 test['Gender'] = label_encoder_gender.transform(test['Gender'])
+
+label_encoder_class = LabelEncoder()
+train['Class'] = label_encoder_class.fit_transform(train['Class'])
+test['Class'] = label_encoder_class.transform(test['Class'])
 
 label_encoder_customer_type = LabelEncoder()
 train['Customer Type'] = label_encoder_customer_type.fit_transform(train['Customer Type'])
@@ -40,20 +56,28 @@ label_encoder_travel_type = LabelEncoder()
 train['Type of Travel'] = label_encoder_travel_type.fit_transform(train['Type of Travel'])
 test['Type of Travel'] = label_encoder_travel_type.transform(test['Type of Travel'])
 
-label_encoder_class = LabelEncoder()
-train['Class'] = label_encoder_class.fit_transform(train['Class'])
-test['Class'] = label_encoder_class.transform(test['Class'])
+#kodowanie float na liczby całkowite
+test['Arrival Delay in Minutes'] = test['Arrival Delay in Minutes'].astype(int)
+train['Arrival Delay in Minutes'] = train['Arrival Delay in Minutes'].astype(int)
 
-X_train = train.drop(['id', 'satisfaction'], axis=1)
+#zamiana wartosci na 0 i 1
+label_encoder_satisfaction = LabelEncoder()
+train['satisfaction'] = label_encoder_satisfaction.fit_transform(train['satisfaction'])
+test['satisfaction'] = label_encoder_satisfaction.transform(test['satisfaction'])
+
+
+print(test.head())
+print(train.head())
+
+X_train = train.drop(['satisfaction'], axis=1)
 y_train = train['satisfaction']
 
-X_test = test.drop(['id'], axis=1)
-
-y_train = pd.get_dummies(y_train)
+X_test = test
 
 scaler = StandardScaler()
 X_train[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']] = scaler.fit_transform(X_train[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']])
 X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']] = scaler.transform(X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']])
+
 
 model = keras.models.Sequential()
 model.add(keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)))
@@ -62,16 +86,16 @@ model.add(keras.layers.Dense(2, activation='softmax'))
 
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=50, validation_split=0.2)
+model.fit(X_train, y_train, epochs=10, validation_split=0.2)
 
-model.save("my_model.h5")
+# model.save("my_model.h5")
 
-loaded_model = keras.models.load_model("my_model.h5")
+# loaded_model = keras.models.load_model("my_model.h5")
 
-X_test = test.drop(['id', 'satisfaction'], axis=1)
-X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']] = scaler.transform(X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']])
+# X_test = test.drop(['id', 'satisfaction'], axis=1)
+# X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']] = scaler.transform(X_test[['Age', 'Flight Distance', 'Departure Delay in Minutes', 'Arrival Delay in Minutes']])
 
-predictions = loaded_model.predict(X_test)
+# predictions = loaded_model.predict(X_test)
 
 
 
